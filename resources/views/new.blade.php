3cec4a6874d318a9db8d913e8acf80ae9200bf9c
eritori <7eritori8@gmail.com>
2020-02-27 19:56:43 +0900

    整形

--- resources/views/new.blade.php
+++ resources/views/new.blade.php
@@ -2,22 +2,31 @@
 @section('content')
     新規作成ぺーじ
 
-<form  method="post" action="/confirm">
-    {{ csrf_field() }}
-    <p>料理名<spam>*</spam></p> <input type="text" name="name">
-    <p>材料<spam>*</spam></p><textarea name="material"></textarea>
-    <p>作り方<spam>*</spam></p><textarea name ="process"></textarea>
+    <form method="post" action="/confirm">
+        {{ csrf_field() }}
+        <p>料理名
+            <spam>*</spam>
+        </p>
+        <input type="text" name="name">
+        <p>材料
+            <spam>*</spam>
+        </p>
+        <textarea name="material"></textarea>
+        <p>作り方
+            <spam>*</spam>
+        </p>
+        <textarea name="process"></textarea>
 
 
-    <input type="submit" value="完成">
-</form>
-@if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
+        <input type="submit" value="完成">
+    </form>
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
 @endsection
