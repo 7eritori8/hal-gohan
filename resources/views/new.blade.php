d192b261389eeececef85a4b97e07f908dd682f4
eritori <7eritori8@gmail.com>
2020-02-27 20:05:08 +0900

    homeを微修正

--- resources/views/new.blade.php
+++ resources/views/new.blade.php
@@ -1,25 +1,35 @@
 @extends('layouts.app')
 @section('content')
-    新規作成ぺーじ
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">新規作成ぺーじ</div>
+                <div class="card-body">
+                    <form method="post" action="/confirm">
+                        {{ csrf_field() }}
+                        <p>料理名
+                            <spam>*</spam>
+                        </p>
+                        <input type="text" name="name">
+                        <p>材料
+                            <spam>*</spam>
+                        </p>
+                        <textarea name="material"></textarea>
+                        <p>作り方
+                            <spam>*</spam>
+                        </p>
+                        <textarea name="process"></textarea>
 
-    <form method="post" action="/confirm">
-        {{ csrf_field() }}
-        <p>料理名
-            <spam>*</spam>
-        </p>
-        <input type="text" name="name">
-        <p>材料
-            <spam>*</spam>
-        </p>
-        <textarea name="material"></textarea>
-        <p>作り方
-            <spam>*</spam>
-        </p>
-        <textarea name="process"></textarea>
+
+                        <input type="submit" value="完成">
+                    </form>
+                </div>
+
+        </div>
+    </div>
 
 
-        <input type="submit" value="完成">
-    </form>
+
     @if ($errors->any())
         <div class="alert alert-danger">
             <ul>
