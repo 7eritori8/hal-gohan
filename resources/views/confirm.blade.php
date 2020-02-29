28425f96f0777b3be4e9976f90427b7169e8ae40
eritori <7eritori8@gmail.com>
2020-03-01 01:18:22 +0900

    確認ページの見た目を修正

--- resources/views/confirm.blade.php
+++ resources/views/confirm.blade.php
@@ -1,11 +1,25 @@
 @extends('layouts.app')
 @section('content')
-<h2>かくにんページ</h2>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">確認ページ</div>
+                    <div class="card-body">
+                        料理名：{{ $name }}<br>
+                        材料：{{$material}}<br>
+                        作り方：{{$process}}<br>
+                        <form action="/recipe">
+                            <input type="submit" value="OK">
+                        </form>
 
-料理名{{ $name }}<br>
-材料{{$material}}<br>
-作り方{{$process}}<br>
-<form action="/recipe">
-<input type="submit" value="OK">
-</form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <h2></h2>
+
+
+
 @endsection
