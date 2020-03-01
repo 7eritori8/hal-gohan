6eb324b22f8c3def36b6f03a12510b1e789836a2
eritori <7eritori8@gmail.com>
2020-03-02 01:56:35 +0900

    修正用ページ、修正後確認ページの見た目を修正

--- resources/views/update.blade.php
+++ resources/views/update.blade.php
@@ -1,8 +1,22 @@
 @extends('layouts.app')
 @section('content')
-以下のように修正しました。確認してください。<br>
-{{$name}}<br>
-{{$process}}<br>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">修正後確認ページ</div>
+                    <div class="card-body">
+                        以下のように修正しました。確認してください。<br>
+                        {{$name}}<br>
+                        {{$process}}<br>
 
-<a href="/">home</a>
+                        <a href="/">home</a>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 @endsection
--- resources/views/updateform.blade.php
+++ resources/views/updateform.blade.php
@@ -1,14 +1,26 @@
 {{--updateform(修正用入力ページ)--}}
 @extends('layouts.app')
 @section('content')
-<form  method="post" action="/update">
-    {{ csrf_field() }}
-    <input type="hidden" name="id" value="{{$id}}">
-    <p>料理名<span>*</span></p> <input type="text" name="name" value="{{$name}}">
-    <p>材料<span>*</span></p><textarea name="material"></textarea>
-    <p>作り方<span>*</span></p><textarea name ="process">"{{$process}}"</textarea>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">修正ページ</div>
+                    <div class="card-body">
+                        <form method="post" action="/update">
+                            {{ csrf_field() }}
+                            <input type="hidden" name="id" value="{{$id}}">
+                            <p>料理名<span>*</span></p> <input type="text" name="name" value="{{$name}}">
+                            <p>材料<span>*</span></p><textarea name="material"></textarea>
+                            <p>作り方<span>*</span></p><textarea name="process">"{{$process}}"</textarea>
 
 
-    <input type="submit" value="修正">
-</form>
+                            <input type="submit" value="修正">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    </form>
 @endsection
