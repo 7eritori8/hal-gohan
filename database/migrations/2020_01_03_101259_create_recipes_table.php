15dd93923defa6fa551a46bde0f91959ebac2cb8
eritori <7eritori8@gmail.com>
2020-03-02 01:41:05 +0900

    migrationファイルにmaterialのカラムを追加

--- database/migrations/2020_01_03_101259_create_recipes_table.php
+++ database/migrations/2020_01_03_101259_create_recipes_table.php
@@ -18,6 +18,7 @@ class CreateRecipesTable extends Migration
             $table->timestamps();
             $table->string('name');
             $table->text('process');
+            $table->text('material');
         });
     }
 
